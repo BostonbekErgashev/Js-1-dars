e36f48a7ff5fcf0c536c5199bb010acca04e9f07
Bostonbek Ergashev <bostonbekone1@gmail.com>
2024-08-15 09:20:10 +0500

    Js 1-dars
    Zeromax IT-Center , Web programming
    36-group

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// JSga kirish
+//console.log("Men faqat konsol oynasida ko'rinaman!");
+//alert("Saytimizga xush kelibsiz!");
+//document.write("Men bodyda ko'rinaman!");
+// JS o'zgaruvchilari
+//let x = 5;
+//let b = 47;
+//let x = 15, y = 7, z = 10;
+//number
+//string
+//let son = 45;
+//let matn ="JavaScript"
+//document.write( typeof matn);
+//let x = 714, b = 41, j = 2, like = 98589, job =14;
+//document.write(714*41*2-98589/14)
+// JS matematika 
+let x = 78;
+//x += 6;     x=5+5    qo'shish
+//x -= 5;     x=5-5    ayirish
+//x *= 5;     x=5*5    ko'paytirish
+//x /= 2;     x=5/5    bo'lish
+//let qoldiq =x%12;    qoldiq
+//let daraja =x**5;    daraja
+//++x;
+//--x;
+//x++;
+//x--;
+
+
+document.write(x);
